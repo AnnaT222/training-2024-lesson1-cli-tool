eca4015019959d3ddb7a65ca949f6cf96256fa89
AnnaT222 <annatazaian@gmail.com>
2024-07-20 11:07:27 +0400

    initial changes

---
+++ index.js
@@ -0,0 +1,37 @@
+const args = process.argv.slice(2);
+console.log(args);
+
+let name = "user";
+let level = 1;
+let greeting = "Hello";
+
+for (let i = 0; i <= args.length; i++) {
+  switch (args[i]) {
+    case "--name":
+    case "-n":
+      name = args[i + 1];
+      i++;
+      break;
+    case "--level":
+    case "-l":
+      level = parseInt(args[i + 1]);
+      i++;
+      break;
+    case "--greeting":
+    case "-g":
+      greeting = args[i + 1];
+      i++;
+      break;
+    default:
+      break;
+  }
+}
+
+let dateTime = new Date();
+if (level === 1) {
+  console.log(`${name} ${greeting}`);
+} else if (level === 2) {
+  console.log(`${name} ${greeting} (Date and Time: ${dateTime})`);
+} else {
+    console.log(`${name} ${greeting}`);
+}
